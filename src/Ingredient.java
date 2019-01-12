f8c3b85c855aa1d9b6fa5424cdcb469189306bfe
andron13 <andron13@gmail.com>
2019-01-12 13:53:20 +0100

    minor changes before we work together

--- src/Ingredient.java
+++ src/Ingredient.java
@@ -1,3 +1,8 @@
 public class Ingredient {
 
+	private String name;
+	private String descriptions;
+
+	// private weightsAndMeasures wam;
+
 }
--- src/RecepiSteps.java
+++ src/RecepiSteps.java
@@ -1,7 +1,8 @@
 
 public class RecepiSteps {
     private byte stepID;
-    private String text;
+    private String stepTitle;
+    private String stepDescription;
     private Photo photo;
     // private Video video;
     // private Audio audio;
