8d818c71db08012b31b656c525b130c5a8bb5223
Black82 <blackrailean@gmail.com>
2019-01-20 19:58:39 +0100

    restApi

--- src/main/java/App/controler/RecipeController.java
+++ src/main/java/App/controler/RecipeController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-.
+
 @RestController
 public class RecipeController {
     @Autowired
--- src/main/java/App/model/Recipe.java
+++ src/main/java/App/model/Recipe.java
@@ -39,5 +39,5 @@ public class Recipe {
     @ManyToOne( targetEntity = Rate.class )
     private Rate rate;
 
-,
+
 }
