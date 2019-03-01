01d21d8fbf9be89d0c9a7d056edc3e02b518fd53
Black82 <blackrailean@gmail.com>
2019-03-01 14:40:59 +0100

    fixetControlerMaping

--- src/main/java/de/recipe/controller/CocktailController.java
+++ src/main/java/de/recipe/controller/CocktailController.java
@@ -4,11 +4,11 @@ import de.recipe.web.CocktailWeb;
 import de.recipe.web.CocktailWebOutput;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/cocktails")
+
 public class CocktailController {
 
     private CocktailService cocktailService;
@@ -19,48 +19,47 @@ public class CocktailController {
 
     }
 
-    @GetMapping( value = "/" )
+    @GetMapping( value = "/ " )
     public List getAllRecipe() {
         return cocktailService.getAllCocktail();
     }
 
-    @PostMapping( value = "/" )
+    @PostMapping( value = "/ " )
     public CocktailWebOutput creatRecipeController(@RequestBody CocktailWeb cocktailWeb) {
         cocktailService.creatCocktail(cocktailWeb);
         return (CocktailWebOutput) cocktailService.convertTheeCoctailsIntoAnotherEmbodiment( cocktailWeb,CocktailWebOutput.class);
     }
 
-    @GetMapping( "/{id}" )
+    @GetMapping( "/{id} " )
     public CocktailWebOutput getCocktailById(@PathVariable Long id) {
         return cocktailService.getCocktailById(id);
     }
 
-    @DeleteMapping( value = "/{id}" )
+    @DeleteMapping( value = "/{id} " )
     public void delletCocktailByID(@PathVariable Long id) {
         cocktailService.deleteCocktailById(id);
 
            }
 
-    @GetMapping( "/ingredient/{name_ingredient}" )
+    @GetMapping( "/ingredient/{name_ingredient} " )
     public List findByIngredient(@PathVariable String name_ingredient) {
         return cocktailService.findByIngredientsContaining(name_ingredient);
     }
 
-    @GetMapping( path = "/author/{name_author}" )
+    @GetMapping( path = "/author/{name_author} " )
     public List <CocktailWebOutput> findBYAuthor(@PathVariable String name_author) {
         return cocktailService.findbyAuthor(name_author);
     }
 
-    @GetMapping( path = "/title/{title}" )
+    @GetMapping( path = "/title/{title} " )
     public CocktailWebOutput findByTitle(@PathVariable String title) {
         return cocktailService.findByTitle(title);
 
     }
 
-    @PutMapping( value = "/{id}" )
+    @PutMapping( value = "/{id} " )
     public void updateCocktailById(@RequestBody CocktailWebOutput cocktailWebOutput, @PathVariable Long id) {
         cocktailService.updateCocktail(cocktailWebOutput, id);
     }
 
 }
-.
