bac76fc89a84cc140eb39f211867dc635ff5dbca
Black82 <blackrailean@gmail.com>
2019-03-24 13:35:58 +0100

    refactoryDB

--- src/main/resources/db/migration/V2_0__INSERT.sql
+++ src/main/resources/db/migration/V2_0__INSERT.sql
@@ -1,29 +1,28 @@
-/*INSERT INTO USER(id, name)
+INSERT INTO USER(id, NAME)
 VALUES (1, 'https://www.cocktail-rezepte-4u.de/rezepte/Sex-on-the-Beach.html');
-INSERT INTO photo(id, patch)
-VALUES (1, 'https://cdn.liquor.com/wp-content/uploads/2013/04/ward-eight.jpg');
-insert INTO ingredient(id, TITLE, description)
-VALUES (1, 'Vodka', '4 cl Vodka'),
-       (2, 'Peach liqueur', '4 cl peach liqueur'),
-       (3, ' Cranberry', '8 cl cranberry juice or Cranberry Rectal'),
-       (4, 'Orange juice', '8 cl of orange juice'),
-       (5, 'Ice', 'Ice cubes');
+INSERT INTO photo(ID, TITLE,PATH,ALT)
+VALUES (1, 'First Photo','https://cdn.liquor.com/wp-content/uploads/2013/04/ward-eight.jpg','200x400');
+insert INTO ingredient(ID, TITLE, DESCRIPTION,PHOTO_ID)
+VALUES (1, 'Vodka', '4 cl Vodka',1),
+       (2, 'Peach liqueur', '4 cl peach liqueur',1),
+       (3, ' Cranberry', '8 cl cranberry juice or Cranberry Rectal',1),
+       (4, 'Orange juice', '8 cl of orange juice',1),
+       (5, 'Ice', 'Ice cubes',1);
 
-INSERT INTO cocktail_ingredients(cocktail_ingredients.Cocktail_id, cocktail_ingredients.ingredients_id)
+INSERT INTO cocktail_ingredients(COCKTAIL_ID, INGREDIENT_ID)
 VALUES (1, 1),
        (1, 2),
        (1, 3),
        (1, 4),
        (1, 5);
-INSERT INTO cocktail_image(Cocktail_id, image_id)
-VALUES (1, 1);
-INSERT INTO cocktail(id, title, announce, publicationDate, cookingTime, prepTimeMinute, author_id)
+
+INSERT INTO cocktail(ID, TITLE, ANNOUNCE, CREATED_DATE, COOKING_TIME, PREP_TIME, USER_ID ,PHOTO_ID)
 VALUES (1, 'Sex on the Beach Cocktail Recipe',
         'So I have to say one of the best drink''s I''ve drunk so far has become my favorite drink as well',
-        '2019-03-04', 112, 15, 1);
+        '2019-03-04', 112, 15, 1 ,1);
 
 
-INSERT INTO USER(id, name)
+/*INSERT INTO USER(id, name)
 VALUES (2, 'cocktail-rezepte-4u.de');
 INSERT INTO photo(id, patch)
 VALUES (2, 'https://cdn.liquor.com/wp-content/uploads/2013/04/ward-eight.jpg');
@@ -63,4 +62,4 @@ VALUES (3, 3);
 INSERT INTO cocktail(id, title, announce, publicationDate, cookingTime, prepTimeMinute, author_id)
 VALUES (3, 'Alfie cocktail',
         'Add the vodka, triple sec and pineapple juice together with a few ice cubes to the shaker and shake vigorously. Strain the mixture through the strainer into a cocktail glass.',
-        '2019-03-04', 16, 15, 3);
+        '2019-03-04', 16, 15, 3);*/
